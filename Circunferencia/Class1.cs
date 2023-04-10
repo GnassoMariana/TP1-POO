3321834910a6a663b87bb94fda08322dd65572bf
juan <juan@EQUIPO-EXO>
2023-04-10 17:40:33 -0300

    Agregar archivos de proyecto.

---
+++ Circunferencia/Class1.cs
@@ -0,0 +1,39 @@
+﻿namespace Circunferencia
+{
+    public class Circunferencia
+    {
+        public Circunferencia()
+        {
+
+        }
+
+        public int Radio { get; set; }
+
+        public Circunferencia( int radio )
+        {
+            Radio = radio;
+        }
+
+        public double GetRadio ()
+        {
+            return Radio;
+        }
+
+        public double GetArea ()
+        {
+            return Math.PI * Math.Pow(Radio, 2);
+        }
+
+        public double GetPerimetro()
+        {
+            return 2 * Math.PI * Radio;
+        }
+
+        int nuevoRadio = 0;
+
+        public int CambiarRadio(nuevoRadio)
+        {
+            return Radio = nuevoRadio;
+        }
+    }
+}
